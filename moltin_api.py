14452196cabc570137fa3e34a6ee4e4aa40e2385
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-04-04 19:34:09 +0300

    Add integration with Moltin

---
+++ moltin_api.py
@@ -0,0 +1,257 @@
+import requests
+from slugify import slugify
+
+
+def get_access_token(client_id, client_secret):
+    url = 'https://api.moltin.com/oauth/access_token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_products(access_token):
+    url = 'https://api.moltin.com/v2/products'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product(access_token, product_id):
+    url = f'https://api.moltin.com/v2/products/{product_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_product_main_image_url(access_token, image_id):
+    url = f'https://api.moltin.com/v2/files/{image_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['data']['link']['href']
+
+
+def create_product(access_token, product_id, name, description,
+                   price, slug=None):
+    url = 'https://api.moltin.com/v2/products'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    product_description = {
+        'data': {
+            'type': 'product',
+            'name': name,
+            'slug': slug if slug else slugify(name),
+            'sku': f'sku-{product_id}',
+            'description': description,
+            'manage_stock': False,
+            'price': [
+                {
+                    'amount': int(price) * 100,
+                    'currency': 'RUB',
+                    'includes_tax': True,
+                },
+            ],
+            'status': 'live',
+            'commodity_type': 'physical',
+        },
+    }
+    response = requests.post(url, headers=headers, json=product_description)
+    response.raise_for_status()
+    return response.json()
+
+
+def add_product_main_image(access_token, product_id, image_id):
+    url = f'https://api.moltin.com/v2/products/{product_id}/relationships/main-image'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    image_description = {
+        'data': {
+            'type': 'main_image',
+            'id': image_id,
+        },
+    }
+    response = requests.post(url, headers=headers, json=image_description)
+    response.raise_for_status()
+    return response.json()
+
+
+def delete_product(access_token, product_id):
+    url = f'https://api.moltin.com/v2/products/{product_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.delete(url, headers=headers)
+    return response.ok
+
+
+def get_or_create_cart(access_token, cart_id, currency='RUB'):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'X-MOLTIN-CURRENCY': currency
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_cart_items(access_token, cart_id):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def add_cart_item(access_token, cart_id, item_id,
+                  item_quantity, currency='RUB'):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'X-MOLTIN-CURRENCY': currency
+    }
+
+    cart_item = {
+        'data': {
+            'id': item_id,
+            'type': 'cart_item',
+            'quantity': item_quantity,
+        },
+    }
+    response = requests.post(url, headers=headers, json=cart_item)
+    response.raise_for_status()
+    return response.json()
+
+
+def remove_cart_item(access_token, cart_id, item_id):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items/{item_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.delete(url, headers=headers)
+    return response.ok
+
+
+def get_customer(access_token, customer_id):
+    url = f'https://api.moltin.com/v2/customers/{customer_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_customer(access_token, email, name=None):
+    url = 'https://api.moltin.com/v2/customers'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+
+    customer = {
+        'data': {
+            'type': 'customer',
+            'name': name if name else email.split('@')[0],
+            'email': email,
+        },
+    }
+
+    response = requests.post(url, headers=headers, json=customer)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_file(access_token, file_url):
+    url = 'https://api.moltin.com/v2/files'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    files = {
+        'file_location': (None, file_url),
+    }
+    response = requests.post(url, headers=headers, files=files)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_flow(access_token, name, description, slug=None, enabled=True):
+    url = 'https://api.moltin.com/v2/flows'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    flow_description = {
+        'data': {
+            'type': 'flow',
+            'name': name,
+            'slug': slug if slug else slugify(name),
+            'description': description,
+            'enabled': enabled,
+        },
+    }
+    response = requests.post(url, headers=headers, json=flow_description)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_flow_field(access_token, flow_id, name, field_type, description,
+                      slug=None, required=True, enabled=True):
+    url = 'https://api.moltin.com/v2/fields'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    field_description = {
+        'data': {
+            'type': 'field',
+            'name': name,
+            'slug': slug if slug else slugify(name),
+            'field_type': field_type,
+            'description': description,
+            'required': required,
+            'enabled': enabled,
+            'relationships': {
+                'flow': {
+                    'data': {
+                        'type': 'flow',
+                        'id': flow_id,
+                    },
+                },
+            },
+        },
+    }
+    response = requests.post(url, headers=headers, json=field_description)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_flow_entry(access_token, flow_slug, fields_slug_per_value: dict):
+    url = f'https://api.moltin.com/v2/flows/{flow_slug}/entries'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    entry_description = {
+        'data': {
+            'type': 'entry',
+            **fields_slug_per_value
+        }
+    }
+    response = requests.post(url, headers=headers, json=entry_description)
+    response.raise_for_status()
+    return response.json()
+
